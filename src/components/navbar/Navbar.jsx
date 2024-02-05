c66910bd459acd2d2c50ca8192e639a17f12a9dc
Rakib-Hasan-Rakib <rakibhasanrakib3006@gmail.com>
2024-02-05 21:34:29 +0600

    final commit

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,53 +1,25 @@
-import { HashLink, NavHashLink } from "react-router-hash-link";
+import { NavHashLink } from "react-router-hash-link";
 import "./navbar.css";
 import Headroom from "react-headroom";
-import { useState } from "react";
-import { FaBars } from "react-icons/fa";
-import { AiOutlineCloseCircle } from "react-icons/ai";
 
 const Navbar = () => {
-  const [menuOpen, setMenuOpen] = useState(false);
 
   return (
     <>
       <Headroom>
         <nav className="nav-container text-white md:flex justify-center items-center w-full my-1">
-          <FaBars onClick={() => setMenuOpen(true)} size={28} className="" />
-          <AiOutlineCloseCircle onClick={() => setMenuOpen(false)} />
-          <div className="nav-background my-4 px-6 md:px-12 py-4 rounded-full">
+          <div
+            className={`nav-background my-4 px-6 md:px-12 py-4 rounded-full`}
+          >
             <ul
-              className={`${menuOpen? 'list-none':'hidden'} md:flex justify-center items-center gap-4 lg:gap-8  text-white text-sm`}
+              className={`flex flex-row justify-center items-center gap-4 lg:gap-8  text-white text-sm`}
             >
-              <NavHashLink
-                to="#"
-              >
-                Home
-              </NavHashLink>
-              <NavHashLink
-                to="#about"
-              >
-                About
-              </NavHashLink>
-              <NavHashLink
-                to="#skills"
-              >
-                Skills
-              </NavHashLink>
-              <NavHashLink
-                to="#projects"
-              >
-                Projects
-              </NavHashLink>
-              <NavHashLink
-                to="#info"
-              >
-                Info
-              </NavHashLink>
-              <NavHashLink
-                to="#contact"
-              >
-                Contact
-              </NavHashLink>
+              <NavHashLink to="#">Home</NavHashLink>
+              <NavHashLink to="#about">About</NavHashLink>
+              <NavHashLink to="#skills">Skills</NavHashLink>
+              <NavHashLink to="#projects">Projects</NavHashLink>
+              <NavHashLink to="#info">Info</NavHashLink>
+              <NavHashLink to="#contact">Contact</NavHashLink>
             </ul>
           </div>
         </nav>
